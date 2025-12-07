580612058f6c89197365f1fbe1610e7915e00b15
Bartosz <123322312+nck23@users.noreply.github.com>
2025-12-07 20:04:39 +0100

    rename func

--- day6/2star/include/calc_diag.h
+++ day6/2star/include/calc_diag.h
@@ -1,6 +1,6 @@
 #ifndef CALC_DIAG_H
 #define CALC_DIAG_H
 
-unsigned long long calc_diagonally(const char* filepath, char** ptr_buffer, const int matrix_rows);
+unsigned long long calc_vertically(const char* filepath, char** ptr_buffer, const int matrix_rows);
 
 #endif
